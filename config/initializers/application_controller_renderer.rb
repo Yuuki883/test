70f18595beec0eca5777b7aebcf09387b3d01a02
Yuuki883 <yuukisaitou14@gmail.com>
2020-06-18 21:02:08 +0900

    マイページ #11 Refile.secret_key追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f8571dac18100265bc79fa6a1c294da15f89dbfee444156773c677239357df6733cd95b21c545dfcc332f586ace906052770ae9568b03d431ebfa8ac44144dce'
