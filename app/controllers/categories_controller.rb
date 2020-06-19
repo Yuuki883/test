cfb5945ec37c19b57a1b120cb0b3b9708302f345
Yuuki883 <yuukisaitou14@gmail.com>
2020-06-19 14:21:18 +0900

    カテゴリ設定 #6 カテゴリコントローラ作成

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,44 @@
+class CategoriesController < ApplicationController
+before_action :set_category, only: [:edit, :update, :destroy]
+
+  def index
+    @category = Category.new
+    @categories = Category.all
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "カテゴリを作成しました！"
+    else
+      @categories = Category.all
+      render 'index'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "カテゴリを更新しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, notice: "カテゴリを削除しました！"
+  end
+
+  private
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
