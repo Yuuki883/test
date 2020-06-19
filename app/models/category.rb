ca955acf0c813c2f0aaa7c41d35bef9af56d09b6
Yuuki883 <yuukisaitou14@gmail.com>
2020-06-19 14:24:17 +0900

    カテゴリ設定 #6 リレーション設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  has_many :topic_categories, dependent: :destroy
+  has_many :topics, through: :topic_categories
+  validates :name, presence: true, length: {maximum: 50, minimum: 1}
+
 end
--- app/models/topic_category.rb
+++ app/models/topic_category.rb
@@ -1,2 +1,4 @@
 class TopicCategory < ApplicationRecord
+  belongs_to :topic
+  belongs_to :category
 end
